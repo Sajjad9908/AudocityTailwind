69d488d38a7fe634dff187f51250288dd6f918a8
Sajjad9908 <gulbadan128@gmail.com>
2025-12-18 02:05:14 +0500

    further responsive issues solved

--- audocity/src/component/hero/Hero.jsx
+++ audocity/src/component/hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <>
     <div id='hero' style={{backgroundImage:`linear-gradient(rgba(8,0,58,0.7), rgba(8,0,58,0.7)), url(${hero})`,backgroundSize:'cover',backgroundPosition:'center'}} className='pl-[10%] pr-[5%] hero w-full min-h-[100vh] text-[white] flex flex-col justify-center gap-8'>
     <div className='p-3.5 text-center flex items-center flex-col'>
-        <h1 className='text-[40px] sm:text-[60px] font-semibold'>We Ensure better education for a better world</h1>
+        <h1 className='text-[20px] sm:text-[60px] font-semibold'>We Ensure better education for a better world</h1>
         <p className='max-w-[700px] mt-[10px] mx-auto mb-[20px] leading-5'>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
    
         <button className='bg-[#FFF] text-[#212121] rounded-[30px] border-0 cursor-pointer outline-0 p-3 flex items-center justify-center text-center gap-2'>Explore More <img className='w-5' src={darkArrow}/> </button>
